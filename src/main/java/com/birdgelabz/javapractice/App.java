d3aa20c0207919adcc571ae9c220e8ebe9744b66
himanshu19 <himanshu@goniyo.com>
2020-09-21 08:56:22 +0530

    inital push

---
+++ src/main/java/com/birdgelabz/javapractice/App.java
@@ -0,0 +1,18 @@
+package com.birdgelabz.javapractice;
+
+/**
+ * Hello world!
+ *
+ */
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class App 
+{
+	private static final Logger LOG = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	LOG.info("Hello World");
+    }
+}
